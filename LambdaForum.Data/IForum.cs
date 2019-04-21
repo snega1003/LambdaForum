57df95e6c9f2e2002d3bc54fc63f7fcc89830256
Snezhanna Roshchupkina <snega900@mail.ru>
2019-04-21 19:15:58 +0300

    Add service layer

---
+++ LambdaForum.Data/IForum.cs
@@ -0,0 +1,27 @@
+﻿using LambdaForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaForum.Data
+{
+    public interface IForum
+    {
+        //Some API/implementation of the interaction with the forum entity model
+        //in this application 
+
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+
+
+
+
+
+    }
+}
---
+++ LambdaForum.Service/ForumService.cs
@@ -0,0 +1,59 @@
+﻿using LambdaForum.Data;
+using LambdaForum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaForum.Service
+{
+    public class ForumService : IForum
+    {
+        //Forum uses any framework to interact with actual data
+
+        private readonly ApplicationDbContext _context;
+
+        //Constructor
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            //return any instance of a forum from database
+            return _context.Forums
+                .Include(forum => forum.Posts); 
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
