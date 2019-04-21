fa1c5c9f93c504e3ff8f559a1c4d5cbcadfba47f
Snezhanna Roshchupkina <snega900@mail.ru>
2019-04-21 04:05:04 +0300

    bug fixed in inject

--- LambdaForum.Data/Models/ApplicationUser.cs
+++ LambdaForum.Data/Models/ApplicationUser.cs
@@ -5,5 +5,6 @@ namespace LambdaForum.Data.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public virtual string Mail_not_necessary { get; set; }
     }
 }
