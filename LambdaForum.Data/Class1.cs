e5a3b8b856aecb717ac5639b739ab3af65989d97
Snezhanna Roshchupkina <snega900@mail.ru>
2019-04-21 03:23:56 +0300

    Migration Commit

---
+++ LambdaForum.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LambdaForum.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ LambdaForum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LambdaForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
