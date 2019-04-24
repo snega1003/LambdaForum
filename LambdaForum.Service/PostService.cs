09dfcec772232b79c630c16b1a9500cc6fad6ecf
snega1003 <snega900@gmail.com>
2019-04-24 23:24:45 +0300

    Home Controller and View, Post Service

--- LambdaForum.Service/PostService.cs
+++ LambdaForum.Service/PostService.cs
@@ -37,7 +37,10 @@ namespace LambdaForum.Service
 
         public IEnumerable<Post> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Posts
+                .Include(post => post.User)
+                .Include(post => post.Replies).ThenInclude(reply => reply.User)
+                .Include(post => post.Forum);
         }
 
         public Post GetById(int id)
@@ -52,6 +55,11 @@ namespace LambdaForum.Service
         public IEnumerable<Post> GetFilteredPosts(string searchQuery)
         {
             throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetLatestPosts(int n)
+        {
+            return GetAll().OrderByDescending(post => post.Created).Take(n);
         }
 
         public IEnumerable<Post> GetPostsByForum(int id)
--- LambdaForum/Controllers/HomeController.cs
+++ LambdaForum/Controllers/HomeController.cs
@@ -1,14 +1,63 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using LambdaForum.Models;
+using LambdaForum.Models.Home;
+using LambdaForum.Data;
+using System.Linq;
+using LambdaForum.Models.Post;
+using LambdaForum.Data.Models;
+using LambdaForum.Models.Forum;
+using System;
 
 namespace LambdaForum.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPost _postService;
+
+        public HomeController(IPost postService)
+        {
+            _postService = postService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = BuildHomeIndexModel();
+            return View(model);
+        }
+
+        private HomeIndexModel BuildHomeIndexModel()
+        {
+            var latestPosts = _postService.GetLatestPosts(10);
+
+            var posts = latestPosts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                AuthorRating = post.User.Rating,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = post.Replies.Count(),
+                Forum = GetForumListingForPost(post)
+            });
+            return new HomeIndexModel
+            {
+                LatestPosts = posts,
+                SearchQuery = ""
+            };
+        }
+
+        private ForumListingModel GetForumListingForPost(Post post)
+        {
+            var forum = post.Forum;
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                Name = forum.Title,
+                //Description = forum.Description,
+                ImageUrl = forum.ImageUrl
+            };
         }
 
         public IActionResult About()
