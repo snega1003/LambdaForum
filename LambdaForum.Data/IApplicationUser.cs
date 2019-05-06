6ed3f1c14693475fb071089375f8570ed141268c
snega1003 <snega900@gmail.com>
2019-05-06 23:23:05 +0300

    Create Profile Controller, AppUser and Upload services

---
+++ LambdaForum.Data/IApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using LambdaForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaForum.Data
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+
+        Task SetProfileImage(string id, Uri uri);
+        Task IncrementRating(string id, Type type);
+    }
+}
---
+++ LambdaForum/Controllers/ProfileController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LambdaForum.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LambdaForum.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+
+        public ProfileController(UserManager<ApplicationUser> userManager, 
+            IApplicationUser userService, 
+            IUpload uploadService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _uploadService = uploadService;
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
